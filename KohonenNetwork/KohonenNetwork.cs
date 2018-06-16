2184bd74eb431ae94bbbe8c6371ed4f1a34d6246
A1essandro <alexander.yermakov.1990@gmail.com>
2018-06-16 12:33:00 +0400

    KohonenNetwork now extends Network from NeuralNetworkConstructor

--- KohonenNetwork/KohonenNetwork.cs
+++ KohonenNetwork/KohonenNetwork.cs
@@ -10,68 +10,32 @@ using NeuralNetworkConstructor.Network.Node.ActivationFunction;
 
 namespace KohonenNetwork
 {
-    public class KohonenNetwork : INetwork
+    public class KohonenNetwork : Network
     {
 
-        private const string OUTPUT_LAYER_ID = "output_layer";
+        private ILayer<INode> _inputLayer = new InputLayer();
+        private ILayer<INode> _outputLayer = new Layer();
 
-        private readonly INetwork _network;
+        public override ICollection<ILayer<INode>> Layers => new ILayer<INode>[] { _outputLayer };
+        public override ILayer<INode> OutputLayer => _outputLayer;
 
         public KohonenNetwork(int inputNodes, int outputNodes, bool withBias = true)
         {
-            var constructor = new NetworkConstructor<Network>();
+            for (var i = 0; i < inputNodes; i++)
+            {
+                _inputLayer.Nodes.Add(new InputNode());
+            }
+
+            if (withBias)
+            {
+                _inputLayer.Nodes.Add(new InputBias());
+            }
 
             for (var i = 0; i < inputNodes; i++)
             {
-                constructor.AddInputNode<InputNode>(Guid.NewGuid().ToString());
+                _outputLayer.Nodes.Add(new Neuron<Gaussian>());
             }
-            if (withBias)
-            {
-                constructor.AddInputNode<InputBias>(Guid.NewGuid().ToString());
-            }
-            constructor.AddLayer<Layer>(OUTPUT_LAYER_ID);
-            for (var i = 0; i < outputNodes; i++)
-            {
-                constructor.AddNeuron<Neuron<Gaussian>>(Guid.NewGuid().ToString());
-            }
-
-            _network = constructor.Complete();
         }
 
-        public IInputLayer InputLayer => _network.InputLayer;
-
-        public ICollection<ILayer<INode>> Layers => _network.Layers.ToArray();
-
-        public ILayer<INode> OutputLayer => _network.OutputLayer;
-
-        public event Action<IEnumerable<double>> OnOutput;
-        public event Action<IEnumerable<double>> OnInput;
-
-        public void Input(IEnumerable<double> input)
-        {
-            OnInput?.Invoke(input);
-            _network.Input(input);
-        }
-
-        public IEnumerable<double> Output()
-        {
-            var result = _network.Output();
-            OnOutput?.Invoke(result);
-
-            return result;
-        }
-
-        public async Task<IEnumerable<double>> OutputAsync()
-        {
-            var result = await OutputAsync();
-            OnOutput?.Invoke(result);
-
-            return result;
-        }
-
-        public void Refresh()
-        {
-            _network.Refresh();
-        }
     }
 }
